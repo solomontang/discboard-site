b63b0a15ab89eb685773fb6b6ed321442e8e69eb
Solomon Tang <solomoncztang@gmail.com>
2017-12-19 19:41:40 -0800

    Setup scaffolding

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+const config = require('config')['knex'];
+
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    mochaTest: {
+      test: {
+        options: {
+          reporter: 'spec'
+        },
+        src: ['server/test/**/*.js']
+      }
+    },
+
+    pgcreatedb: {
+      default: {
+        connection: {
+          user: config.connection.user,
+          password: config.connection.password,
+          host: config.connection.host,
+          port: config.connection.port,
+          database: 'template1'
+        },
+        name: config.connection.database
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-mocha-test');
+  grunt.loadNpmTasks('grunt-pg');
+
+  grunt.registerTask('test', ['mochaTest']);
+};
